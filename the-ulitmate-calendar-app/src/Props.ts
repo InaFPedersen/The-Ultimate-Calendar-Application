8ccb19a4b76bfa0341784b97a3474963e772283b
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2025-01-14 09:11:54 +0100

    refactor: make props file to a interface

--- the-ulitmate-calendar-app/src/Props.ts
+++
@@ -1,4 +0,0 @@
-export type Props = {
-  isAuthenticated: boolean,
-  setIsAuthenticated: any
-}
---
+++ the-ulitmate-calendar-app/src/interfaces/GlobalProps.ts
@@ -0,0 +1,6 @@
+export default interface GlobalProps {
+  isAuthenticated: boolean,
+  setIsAuthenticated: any,
+  activeLanguage: string,
+  setActiveLanguage: any
+}
